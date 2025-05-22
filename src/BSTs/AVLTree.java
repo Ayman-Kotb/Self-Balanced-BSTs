b72ecaa6a128f55698c8641e9c79fdde5da2f185
Ayman-Kotb <aymannkotb@gmail.com>
2025-05-22 11:00:49 +0300

    AVL should be done & tested well

--- src/BSTs/AVLTree.java
+++ src/BSTs/AVLTree.java
@@ -1,31 +1,179 @@
 package BSTs;
 
+import javax.swing.*;
+
 public class AVLTree<T extends Comparable<T>> implements BSTOperations<T> {
     private Node<T> root;
     private int size;
     private int height;
-
     public AVLTree() {
         this.root = null;
         this.size = 0;
     }
+    private int height(Node<T> node) {
+        return node == null ? 0 : node.height;
+    }
+    private int getBalance(Node<T> node) {
+        return (node == null) ? 0 : height(node.left) - height(node.right);
+    }
+    public Node<T> RRotation(Node<T> x) {
+        Node<T> y = x.left;
+        Node<T> z = y.right;
+        y.right = x;
+        x.left = z;
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+        y.height =  Math.max(height(y.left), height(y.right)) + 1;
+        return y ;
+    }
+
+    public Node<T> LRotation(Node<T> x) {
+        Node<T> y = x.right;
+        Node<T> z = y.left;
+        y.left = x;
+        x.right = z;
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+        return y ;
+    }
+
+    public boolean isBalanced(Node<T> x) {
+        if (x == null) return true;
+        return Math.abs(height(x.left) - height(x.right)) <= 1;
+    }
+
 
     @Override
     public boolean insert(T key) {
         // To be implemented
-        return false;
+        if (key == null){
+            return false ;
+        }
+        Node<T> x = new Node<>(key);
+        root = recursiveInsert(root, x);
+        size++ ;
+        return true;
     }
+    public Node<T> recursiveInsert(Node<T> root, Node<T> y) {
+        if (root == null) {
+            root = y;
+            return y;
+        }
+        int cmp = y.key.compareTo(root.key);
+        if (cmp < 0) {
+            root.left = recursiveInsert(root.left, y);
+
+        }
+        else if (cmp > 0) {
+            root.right = recursiveInsert(root.right, y);
+        }
+        else {
+            return root;
+        }
+        root.height = Math.max(height(root.left), height(root.right)) + 1;
+        int balance = getBalance(root);
+        if (balance > 1 && root.left.key.compareTo(y.key) >0) { // LL
+            return RRotation(root);
+        }
+        else if (balance < -1 && root.right.key.compareTo(y.key) <0) { // RR
+            return LRotation(root);
+        }
+        else if (balance > 1 && root.left.key.compareTo(y.key)<0) { // LR
+            root.left = LRotation(root.left);
+            return RRotation(root);
+        }
+        else if (balance < -1 && root.right.key.compareTo( y.key)>0) { // RL
+            root.right = RRotation(root.right);
+            return LRotation(root);
+        }
+        return root;
+    }
+
 
     @Override
     public boolean delete(T key) {
         // To be implemented
-        return false;
+        if (key == null){
+            return false ;
+        }
+        Node<T> x = new Node<>(key);
+        root = recursiveDelete(root , x);
+        size-- ;
+        return true;
+    }
+    public Node<T> findMin(Node<T> root) {
+        if (root.left == null) {
+            return root;
+        }
+        return findMin(root.left);
+    }
+    public Node<T> recursiveDelete(Node<T> root , Node<T> x) {
+        if (root == null){
+            return null ;
+        }
+        int cmp = x.key.compareTo(root.key);
+        if (cmp < 0){
+            root.left = recursiveDelete(root.left , x);
+        }
+        else if (cmp>0){
+            root.right = recursiveDelete(root.right , x);
+        }
+        else { // equal keys
+            if (root.left == null && root.right == null){ // no children
+                return null ;
+            }
+            else if (root.left == null ){ // only  right child
+                return root.right ;
+            }
+            else if (root.right == null){ // only left child
+                return root.left ;
+            }
+            else { // 2 children
+                Node<T> min = findMin(root.right);
+                root.key = min.key;
+                root.right = recursiveDelete(root.right , min);
+            }
+        }
+        root.height = Math.max(height(root.left), height(root.right)) + 1;
+        int balance = getBalance(root);
+        if (balance > 1 && height(root.left.left) > height(root.left.right)) { // LL
+            return RRotation(root);
+        }
+        else if (balance < -1 && height(root.right.right) > height(root.right.left)) { // RR
+            return LRotation(root);
+        }
+        else if (balance > 1 && height(root.left.right) > height(root.left.left)) { // LR
+            root.left = LRotation(root.left);
+            return RRotation(root);
+        }
+        else if (balance < -1 && height(root.right.left) > height(root.right.right)) { // RL
+            root.right = RRotation(root.right); // RR
+            return LRotation(root);           // LL
+        }
+        return root;
     }
 
     @Override
     public boolean search(T key) {
-        // To be implemented
-        return false;
+        if (key == null){
+            return false ;
+        }
+        Node<T> x = new Node<>(key);
+        return recursiveSearch(root , x);
+    }
+    public boolean recursiveSearch(Node<T> root , Node<T> x) {
+        if (root == null){
+            return false ;
+        }
+        int cmp = x.key.compareTo(root.key);
+        if (cmp == 0){
+            return true ;
+        }
+        else if (cmp < 0){
+            return recursiveSearch(root.left , x);
+        }
+        else {
+            return recursiveSearch(root.right , x);
+        }
     }
 
     @Override
@@ -37,4 +185,13 @@ public class AVLTree<T extends Comparable<T>> implements BSTOperations<T> {
     public int height() {
         return root == null ? 0 : root.height;
     }
+    public void visualize() {
+        JFrame frame = new JFrame("AVL Tree Visualization");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new TreeVisualizer<>(root));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import BSTs.AVLTree;
 import BSTs.RedBlackTree;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("NO main");
         RedBlackTree<Integer> riri = new RedBlackTree<>();
+        AVLTree<Integer> IamJustATree = new AVLTree<>();
         System.out.println("hamada");
         for (int i = 1; i <= 15; i++) {
             riri.insert(i);
@@ -34,6 +36,35 @@ public class Main {
         System.out.print(riri.size());
         System.out.print(riri.search(14));
         System.out.print(riri.search(16));
+
+        for (int i = 1; i <= 15; i++) {
+            IamJustATree.insert(i);
+        }
+
+        IamJustATree.insert(50);
+        IamJustATree.insert(60);
+        IamJustATree.insert(61);
+        IamJustATree.insert(62);
+        IamJustATree.insert(16);
+        System.out.print("\n"+IamJustATree.height()+"\n");
+        System.out.print(IamJustATree.size()+"\n");
+
+
+        IamJustATree.delete(50);
+        IamJustATree.delete(13);
+        IamJustATree.delete(60);
+        IamJustATree.delete(100);
+        IamJustATree.delete(14);
+        IamJustATree.delete(10);
+        IamJustATree.delete(14);
+        IamJustATree.delete(10);
+        System.out.print("\n"+IamJustATree.height()+"\n");
+        System.out.print(IamJustATree.size());
+        System.out.print(IamJustATree.search(14));
+        System.out.print("\n"+IamJustATree.search(16));
+        IamJustATree.visualize();
+
+
 
 
 
